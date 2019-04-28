6ec43b10cfc10fac93067403269da60d6716320e
Roshan Gade <roshangade@gmail.com>
2019-04-29 00:47:13 +0530

    Added map of functions execution with error return. Removed unwanted series and waterfall

--- async.go
+++ async.go
@@ -5,34 +5,30 @@
  */
 package async
 
-type Task func() interface{}
-type STask func(interface{}) interface{}
-type Channel chan interface{}
-type Result []interface{}
+type Tasks []func() interface{}
+type ETasks map[string]func() (interface{}, error)
 
 // Reference: https://blog.golang.org/pipelines
 // Reference: https://en.wikipedia.org/wiki/Moore%27s_law
-// https://en.wikipedia.org/wiki/Amdahl%27s_law
+// Reference: https://en.wikipedia.org/wiki/Amdahl%27s_law
 
 type Promise struct {
-	//TODO:
-	//Context - Cancel, Timeout
-	//Concurrency vs Parallelism
+	//TODO: Context - Cancel, Timeout
+	//Rob Pike: Concurrency enables Parallelism
 	//What to do on error? Is it best way to abort all operation?
-	//Can I use map for tasks? It will be easy to associate results with keys
 }
 
 //
 // parallel execution of all tasks
 //
-func (p *Promise) Parallel(tasks []Task) Result {
+func (p *Promise) Parallel(tasks []func() interface{}) []interface{} {
 	// can not use buffer channels, because not able to maintain output sequence
 	// using slice channels
-	workers := make([]Channel, 0, len(tasks))
+	workers := make([]chan interface{}, 0, len(tasks))
 	for _, task := range tasks {
-		workers = append(workers, func() Channel {
-			out := make(Channel)
-			go func(task Task) {
+		workers = append(workers, func() chan interface{} {
+			out := make(chan interface{})
+			go func(task func() interface{}) {
 				defer close(out)
 				out <- task()
 			}(task)
@@ -41,7 +37,7 @@ func (p *Promise) Parallel(tasks []Task) Result {
 	}
 
 	// gather data from all channels
-	out := make(Result, 0, len(tasks))
+	out := make([]interface{}, 0, len(tasks))
 	for _, result := range workers {
 		out = append(out, <-result)
 	}
@@ -50,53 +46,53 @@ func (p *Promise) Parallel(tasks []Task) Result {
 }
 
 //
-// sequential execution of all tasks
+// Parallel execution of all tasks, but returns result as well as error
 //
-func (p *Promise) Series(tasks []Task) (out interface{}, err error) {
-	// unbuffered channel
-	worker := make(Channel)
-	defer close(worker)
+func (p *Promise) ParallelWithMap(tasks map[string]func() (interface{}, error)) (map[string]interface{}, map[string]error) {
+	// can not use buffer channels, because not able to maintain output sequence
+	// using slice channels
+	workers := make(map[string]chan interface{})
+	for key, task := range tasks {
+		workers[key] = func() chan interface{} {
+			out := make(chan interface{})
+			go func(task func() (interface{}, error)) {
+				defer close(out)
+				val, err := task()
+				if err != nil {
+					out <- err
+				} else {
+					out <- val
+				}
+			}(task)
+			return out
+		}()
+	}
 
-	for _, task := range tasks {
-		// need to check the benefits of executing tasks on another go routine
-		go func(task Task) {
-			worker <- task()
-		}(task)
+	// gather data from all channels
+	out := make(map[string]interface{})
+	err := make(map[string]error)
+	for key, result := range workers {
+		var _err error
+		_val := <-result
+		if _err = isError(_val); _err != nil {
+			err[key] = _err
+			_val = nil
+		}
+		out[key] = _val
+	}
 
-		out = <-worker
-
-		if err = isError(out); err != nil {
-			out = nil
+	flag := true
+	for _, e := range err {
+		if e != nil {
+			flag = false
 			break
 		}
 	}
-
-	return
-}
-
-//
-// sequential execution of all tasks, but output of all tasks will be input for next task
-//
-func (p *Promise) Waterfall(tasks []STask) (out interface{}, err error) {
-	// unbuffered channel
-	worker := make(Channel)
-	defer close(worker)
-
-	for _, task := range tasks {
-
-		go func(task STask) {
-			worker <- task(out)
-		}(task)
-
-		out = <-worker
-
-		if err = isError(out); err != nil {
-			out = nil
-			break
-		}
+	if flag {
+		err = nil
 	}
 
-	return
+	return out, err
 }
 
 func isError(val interface{}) (err error) {
--- examples/parallel/main.go
+++ examples/parallel/main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println("start")
 	var promise async.Promise
 
-	tasks := []async.Task{
+	tasks := async.Tasks{
 		func() interface{} {
 			fmt.Println("Running Hello..........")
 			time.Sleep(4 * time.Second)
@@ -31,6 +31,29 @@ func main() {
 	fmt.Println("End: ", time.Now())
 
 	fmt.Println("Result: ", result)
+
+	fmt.Println("=============================================================")
+
+	mtasks := async.ETasks{
+		"Hello": func() (interface{}, error) {
+			fmt.Println("Running Hello..........")
+			time.Sleep(4 * time.Second)
+			fmt.Println("Completed Hello..........")
+			return "Hello", nil //errors.New("EHello")
+		},
+		"World": func() (interface{}, error) {
+			fmt.Println("Running World..........")
+			time.Sleep(2 * time.Second)
+			fmt.Println("Completed World..........")
+			return "World", nil //errors.New("EWorld")
+		},
+	}
+
+	fmt.Println("From: ", time.Now())
+	mresult, err := promise.ParallelWithMap(mtasks)
+	fmt.Println("End: ", time.Now())
+
+	fmt.Println("Result: ", mresult, err)
 
 	fmt.Println("exit")
 }
